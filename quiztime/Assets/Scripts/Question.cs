fa29863a826d3efb0c5213430225c997b55ef302
Anthony Lopez <anthony.ron.lopez@gmail.com>
2019-02-09 23:52:32 -0600

    add video file; remove some comments

--- quiztime/Assets/Scripts/Question.cs
+++ quiztime/Assets/Scripts/Question.cs
@@ -7,14 +7,4 @@ public class Question
     public string wrong1;
     public string wrong2;
     public string wrong3;
-    /*
-    Question(string q, string c, string w1, string w2, string w3)
-    {
-        question = q;
-        correct = c;
-        wrong1 = w1;
-        wrong2 = w2;
-        wrong3 = w3;
-    }
-    */
 }
